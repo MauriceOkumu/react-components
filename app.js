564add8cc4e6bcc594a2a28109e3d991f5e50ad6
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-01-06 11:40:01 -0800

    Items rendering

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // TODO
+
+var GroceryList = () => (
+     <ul>ITEMS
+     	<li>Item 1 is rendering</li>
+     	<li>Item 2 is rendering</li>
+     </ul>
+	);
+
+ReactDOM.render(<GroceryList />,document.getElementById("app"));
